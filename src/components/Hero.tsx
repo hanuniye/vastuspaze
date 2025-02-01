8f8f3e8546212579ab1bcf2e3799bcf7e5c264fd
hanuniye <hanuniye@gmail.com>
2025-02-01 20:26:05 +0300

    added services and fix hero issues

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,12 +3,12 @@ import hero from "@/assets/hero.webp";
 
 const Hero = () => {
   return (
-    <div className="max-w-7xl mx-auto py-20">
+    <div className="max-w-7xl mx-auto py-12  border-b-2 lg:py-20">
       <div className="flex flex-col items-center">
-        <h1 className="uppercase font-bold text-[60px] lg:text-[160px] p-0">
+        <h1 className="uppercase font-bold text-[50px] lg:text-[160px] p-0">
           {HERO_CONTENT.title}
         </h1>
-        <p className="font-medium mb-4 p-0 tracking-tighter text-lg">
+        <p className="font-medium mb-4 tracking-tighter text-center text-base md:text-lg ">
           {HERO_CONTENT.subtitle}
         </p>
         <img
